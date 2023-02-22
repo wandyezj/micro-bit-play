6de0130002e288990364ba9f5b0be7eaf347ec5a
wandyezj <wandyezj@gmail.com>
2023-02-21 19:51:35 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
+// Sleep (do nothing) for number of milliseconds.
+function sleep (milliseconds: number) {
+    stamp = control.millis()
+    // current time - previous time = time passed
+    while (control.millis() - stamp < milliseconds) {
+        control.waitMicros(1000)
+    }
+}
+// turns pin 0 on and off with a half second delay.
+function pulse_pin_0 () {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    sleep(500)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    sleep(500)
+}
+let stamp = 0
+// version number
+// 
+// tell designs apart, and when they are loaded.
+basic.showNumber(3)
 basic.forever(function () {
-	
+    pulse_pin_0()
 })
